b27b7d73ae9889b7be502ee2ffb0c92e61de8029
RafaGameCreator <107970102+RafaGameCreator@users.noreply.github.com>
2023-12-04 18:52:00 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "XAMUEL";
+const accessCode2 = "MIGUEL O'HARA";
+const accessCode3 = "CEBOLINHA CANTANDO EVIDÊNCIAS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("U E X L M A", 100,50)
+    fill("lightblue")
+    text("Dica: versando", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("H A I G U R ' M O A E L (espaço)", 700,150)
+    fill("lightblue")
+    text("Dica: https://www.youtube.com/watch?v=OgD_TxzjniU", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("Ê N E I B (espaço) A N A O D N S (espaço) C C T A H O D E V L I N I C A", 100,250)
+    fill("lightblue")
+    text("Dica: https://www.youtube.com/watch?v=XtyV2LNlJOg", 100,270)
+
+}
